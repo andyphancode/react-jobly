51e5ee0a73c4337eefb40fe7de9ec6a08c601253
andyphancode <andyphanpxh@gmail.com>
2024-03-27 11:49:26 -0700

    added job applying functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Routing from './components/routing-nav/Routing';
 import NavBar from './components/routing-nav/NavBar';
 import { BrowserRouter as Router} from "react-router-dom";
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import JoblyApi from './api/api';
 import UserContext from './components/UserContext';
 import {jwtDecode} from "jwt-decode";
@@ -13,7 +13,7 @@ function App() {
   const [userInfoLoaded, setUserInfoLoaded] = useState(false);
   const [currentUser, setCurrentUser] = useState(null);
   const [token, setToken] = useLocalStorage("jobly-token");
-
+  const [applications, setApplications] = useState(new Set([]))
 
 
 
@@ -25,6 +25,7 @@ function App() {
           JoblyApi.token = token;
           let currentUser = await JoblyApi.getCurrentUser(username);
           setCurrentUser(currentUser);
+          setApplications(new Set(currentUser.applications))
         } catch (err) {
           console.error(err);
           setCurrentUser(null);
@@ -66,13 +67,26 @@ function App() {
     }
   }
 
+  // Check if job has been applied for
+  function hasAppliedToJob(id) {
+    return applications.has(id);
+  }
+
+  // Check if job has been applied to and update application IDs
+  function apply(id) {
+    if(!hasAppliedToJob(id)) {
+      JoblyApi.applyToJob(currentUser.username, id);
+      setApplications(new Set([...applications, id]));
+    }
+  }
+
   if (!userInfoLoaded) return <LoadingSpinner />;
 
   return (
     <div className="App">
       <Router>
         <UserContext.Provider
-          value={{ currentUser, setCurrentUser}}
+          value={{ currentUser, setCurrentUser, hasAppliedToJob, apply}}
         >
           <NavBar logout={logout}/>
           <main>
