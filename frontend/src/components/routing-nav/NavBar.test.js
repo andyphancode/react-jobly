fd79a75cb1e4dc2675e2269cd5db9c9ef088f807
andyphancode <andyphanpxh@gmail.com>
2024-03-27 09:53:57 -0700

    test updates

--- frontend/src/components/routing-nav/NavBar.test.js
+++ frontend/src/components/routing-nav/NavBar.test.js
@@ -2,12 +2,15 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import NavBar from "./NavBar";
+import UserContext, { UserProvider } from "../../testProvider";
 
 
 it("renders without crashing", function () {
     render(
         <MemoryRouter>
-            <NavBar />
+            <UserProvider>
+                <NavBar />
+            </UserProvider>
         </MemoryRouter>,
     );
 });
@@ -15,7 +18,9 @@ it("renders without crashing", function () {
 it("matches snapshot", function () {
     const { asFragment } = render(
         <MemoryRouter>
-            <NavBar />
+            <UserProvider>
+                <NavBar />
+            </UserProvider>
         </MemoryRouter>,
     );
     expect(asFragment()).toMatchSnapshot();
