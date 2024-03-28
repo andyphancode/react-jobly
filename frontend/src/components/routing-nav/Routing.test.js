1e62d43523e50bd0fae19598b302f959cfcf03db
andyphancode <andyphanpxh@gmail.com>
2024-03-28 16:54:02 -0700

    updated tests/snapshots

--- frontend/src/components/routing-nav/Routing.test.js
+++ frontend/src/components/routing-nav/Routing.test.js
@@ -2,19 +2,24 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Routing from "./Routing";
 import { MemoryRouter } from "react-router-dom";
+import { UserProvider } from "../../testProvider";
 
 it("renders without crashing", function () {
     render(
         <MemoryRouter>
-            <Routing/>
+            <UserProvider>
+                <Routing/>
+            </UserProvider>
         </MemoryRouter>
     )
 });
 
-it("renders without crashing", function () {
+it("matches snapshot", function () {
     const { asFragment } = render(
         <MemoryRouter>
-            <Routing/>
+            <UserProvider>
+                <Routing/>
+            </UserProvider>
         </MemoryRouter>
     );
     expect(asFragment()).toMatchSnapshot();
