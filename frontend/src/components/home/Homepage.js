2aaf386eeb35c7a34afe50e1c5d24ddac470135e
andyphancode <andyphanpxh@gmail.com>
2024-03-28 16:53:56 -0700

    added different UI based on logged in status + styling for text

--- frontend/src/components/home/Homepage.js
+++ frontend/src/components/home/Homepage.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import "./Homepage.css";
+import {Row, Col, Button} from "reactstrap";
+import UserContext from "../UserContext";
+import { Link } from "react-router-dom";
 
 function Homepage() {
+  const {currentUser} = useContext(UserContext);
+  
   return (
-    <div>
-      <h1>Welcome to our website!</h1>
+    <div className="Homepage">
+      {!currentUser ?
+      <h1>Welcome to Jobly!</h1>
+      :
+      <h1>Welcome back, {currentUser.username}.</h1>
+      }
+      {!currentUser ?
+      <Row className="mt-3">
+        <Col>
+          <Link to="/login">
+            <Button className="home-button me-3" color="primary">
+              Login
+            </Button>          
+          </Link>
+          <Link to="/signup">
+            <Button className="home-button" color="primary">
+              Signup
+            </Button>          
+          </Link>
+        </Col>
+      </Row>
+      :
+      <Row>
+        <Col>
+          <Link to="/companies">
+            <Button className="home-button me-3" color="primary">
+              Find Companies
+            </Button>          
+          </Link>
+          <Link to="/jobs">
+            <Button className="home-button" color="primary">
+              Find Jobs
+            </Button>          
+          </Link>
+        </Col>
+      </Row>    
+      }
+
     </div>
   );
 };
