a59588c9bb7e8510a9d2a8d866c2e8fce26288d0
andyphancode <andyphanpxh@gmail.com>
2024-03-30 23:03:25 -0700

    update for vercel deployment/db change

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,55 @@
+// for development on personal PC
+// "use strict";
+// /** Database setup for jobly. */
+// const { Client } = require("pg");
+// const { getDatabaseUri } = require("./config");
+
+// let db;
+
+
+
+
+// try // windows route
+// {
+//   if (process.env.NODE_ENV === "production") {
+//     db = new Client({
+//       host: "/var/run/postgresql",
+//       database: getDatabaseUri(),
+//       ssl: {
+//         rejectUnauthorized: false
+//       }
+//     });
+//   } else {
+//     db = new Client({
+//       host: "/var/run/postgresql",
+//       database: getDatabaseUri()
+//     });
+//   }
+//   db.connect();
+
+//   module.exports = db;
+// } 
+// catch // mac route
+// {
+//   if (process.env.NODE_ENV === "production") {
+//     db = new Client({
+//       // connectionString: `postgresql:///${getDatabaseUri()}`,
+//       connectionString: getDatabaseUri(),
+//       ssl: {
+//         rejectUnauthorized: false
+//       }
+//     });
+//   } else {
+//     db = new Client({
+//       // connectionString: `postgresql:///${getDatabaseUri()}`
+//       connectionString: getDatabaseUri()
+//     });
+//   }
+//   db.connect();
+
+//   module.exports = db;
+// }
+
 "use strict";
 /** Database setup for jobly. */
 const { Client } = require("pg");
@@ -5,46 +57,22 @@ const { getDatabaseUri } = require("./config");
 
 let db;
 
-
-
-
-try // windows route
-{
+try {
   if (process.env.NODE_ENV === "production") {
     db = new Client({
-      host: "/var/run/postgresql",
-      database: getDatabaseUri(),
+      connectionString: process.env.DATABASE_URL,
       ssl: {
         rejectUnauthorized: false
       }
     });
   } else {
     db = new Client({
-      host: "/var/run/postgresql",
-      database: getDatabaseUri()
-    });
-  }
-  db.connect();
-
-  module.exports = db;
-} 
-catch // mac route
-{
-  if (process.env.NODE_ENV === "production") {
-    db = new Client({
-      // connectionString: `postgresql:///${getDatabaseUri()}`,
-      connectionString: getDatabaseUri(),
-      ssl: {
-        rejectUnauthorized: false
-      }
-    });
-  } else {
-    db = new Client({
-      // connectionString: `postgresql:///${getDatabaseUri()}`
       connectionString: getDatabaseUri()
     });
   }
   db.connect();
-
-  module.exports = db;
+} catch (error) {
+  console.error("Error connecting to the database:", error);
 }
+
+module.exports = db;
